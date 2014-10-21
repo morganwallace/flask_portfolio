ac7e562fb67342f3e16236c7f731569172bb7158
Morgan Wallace <morgan@ischool.berkeley.edu>
2014-10-21 16:09:05 -0700

    fixed responsiveness of background image

--- static/js/main.js
+++ static/js/main.js
@@ -63,27 +63,86 @@ $(window).load(function() {
 	var theWindow        = $(window),
 	    $bg              = $("#bg"),
 	    aspectRatio      = $bg.width() / $bg.height();
+    var topImageArea= 200+$(".navbar").height();
 
 	// $("#projectsViewer").css(
 	// 	"margin-top",theWindow.height()-400
 	// )
 	    			    		
 	function resizeBg() {
-		
-		if ( (theWindow.width() / theWindow.height()) > aspectRatio ) {
-		    $bg
+		//if in mobile view
+		if ($("#projectsViewer").width()>=theWindow.width()) {
+			console.log("mobile view");
+			if ((theWindow.width() / topImageArea) > aspectRatio ){
+				console.log("mini width");
+				$bg
 		    	.removeClass()
-		    	.addClass('bgheight');
-		    	// $("#imgWrapper").css("height",theWindow.height());
-
-		} else {
-		    $bg
-		    	.removeClass()
+		    	.attr("style","")
 		    	.addClass('bgwidth');
-		    	// $("#imgWrapper").css("height",theWindow.height());
-		}
-					
+			}else{
+				console.log("mini height");
+				$bg
+		    	.removeClass()
+		    	// .addClass('bgheight');
+		    	.height(topImageArea);
+		    };
+			}
+			// if in desktop view
+			else{
+				console.log("desktop view");
+				if ( (theWindow.width() / theWindow.height()) < aspectRatio ) {
+				    
+				    $bg
+				    	.removeClass()
+				    	.attr("style","")
+				    	.addClass('bgheight');
+				    	// $("#imgWrapper").css("height",theWindow.height());
+				    	console.log('height');
+				    	if (theWindow.width()>$bg.width()) {
+				    		$bg
+						    	.removeClass()
+						    	.attr("style","")
+						    	.addClass('bgwidth');
+						    	// $("#imgWrapper").css("height",theWindow.height());
+						    	console.log('width');
+						    	};
+
+				} else {
+				    $bg
+				    	.removeClass()
+				    	.attr("style","")
+				    	.addClass('bgwidth');
+				    	// $("#imgWrapper").css("height",theWindow.height());
+				    	console.log('width');
+				}
+				// console.log($("#projectsViewer").css("margin-top")+$(".navbar").height());
+
+			}
+		// if (topImageArea> $bg.height()){
+			
+			
+		// }else{
+		// 	console.log('width');
+		// };
+		
 	}
+		
+	// 	if ( (theWindow.width() / theWindow.height()) > aspectRatio ) {
+	// 	    $bg
+	// 	    	.removeClass()
+	// 	    	.addClass('bgheight');
+	// 	    	// $("#imgWrapper").css("height",theWindow.height());
+	// 	    	console.log('height');
+
+	// 	} else {
+	// 	    $bg
+	// 	    	.removeClass()
+	// 	    	.addClass('bgwidth');
+	// 	    	// $("#imgWrapper").css("height",theWindow.height());
+	// 	    	console.log('width');
+	// 	}
+					
+	// }
 	                   			
 	theWindow.resize(resizeBg).trigger("resize");
 
