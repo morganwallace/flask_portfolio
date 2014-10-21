4b68b72d0537d909e89f72ae5147978565f87230
Morgan Wallace <morgan@ischool.berkeley.edu>
2014-10-21 16:50:17 -0700

    making app smaller in hopes it'll work on heroku

--- app.py
+++ app.py
@@ -18,64 +18,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://ayzcctpvbmwajn:z1bTq5u6fbqWm
 ####### Database classes/schema
 from manage import db, User, Project
 
-# basedir = os.getcwd()
-# login_manager = LoginManager()
-# login_manager.init_app(app)
-# oid = OpenID(app, os.path.join(basedir, 'tmp'))
-
-
-# @login_manager.user_loader
-# def load_user(id):
-#     return User.query.get(int(id))
-
-
-# @app.route("/login", methods=["GET", "POST"])
-# def login():
-#     form = LoginForm()
-#     if form.validate_on_submit():
-#         # login and validate the user...
-#         login_user(user)
-#         flash("Logged in successfully.")
-#         return redirect(request.args.get("next") or url_for("index"))
-#     return render_template("login.html", form=form)
-
-
-# login required decorator
-# def login_required(f):
-#     @wraps(f)
-#     def wrap(*args, **kwargs):
-#         if 'user_id' in request.cookies:
-#             return f(*args, **kwargs)
-#         else:
-#             flash('You need to login first.')
-#             return redirect(url_for('signup'))
-#     return wrap
-
-
-# @app.route("/logout")
-# @login_required
-# def logout():
-#     logout_user()
-#     return redirect(home)
-
-
-
-# @app.route('/login', methods = ['GET', 'POST'])
-# @oid.loginhandler
-# def login():
-#     if g.user is not None and g.user.is_authenticated():
-#         return redirect(url_for('index'))
-#     form = LoginForm()
-#     if form.validate_on_submit():
-#         session['remember_me'] = form.remember_me.data
-#         return oid.try_login(form.openid.data, ask_for = ['nickname', 'email'])
-#     return render_template('login.html', 
-#         title = 'Sign In',
-#         form = form,
-#         providers = app.config['OPENID_PROVIDERS'])
-
-
-########  Routing
 
 @app.route('/')
 def home():
@@ -93,62 +35,14 @@ def home():
             'imagesLinks':proj[5].split()[0], #only take first photo
             'snippet':proj[6],
             })
-    # app.logger.debug(projectsList)
-    # user_id = request.cookies.get('user_id')
-    # flash('Your user id is: '+user_id)
     return render_template('index.html',
         projectsList=projectsList)
 
-## add admin.html soon
-# @app.route('/admin')
-# def admin():
-#     return render_template('admin.html')
 
 @app.route('/add')
 @login_required
 def add():
     return render_template('add.html')
-
-
-# @app.route('/adduser' , methods=['POST'])
-# def add_user():
-#     user_info=request.form
-#     app.logger.debug(user_info)
-#     user = User(firstname=user_info['firstname'],
-#         lastname=user_info['lastname'],
-#         email=user_info['email'],
-#         username=user_info['username'],
-#         password=password)
-    
-#     #query database for unique email
-#     test = User.query.filter_by(email=user_info['email']).first()
-#     app.logger.debug(test)
-#     if test is None:
-#         #add record for new user
-#         app.logger.debug('new user')
-#         db.session.add(user)
-#         db.session.commit()
-#         resp = make_response(jsonify(success=True))
-
-#         #eventually make the cookie a token
-#         resp.set_cookie({'user_id': user.id,'username':user_info['username'],'fname':user_info['firstname']})
-#     else: 
-#         app.logger.debug('existing user')
-#         resp = make_response(jsonify(success=False,existing_email=user_info['email']))
-#     return resp
-    #if user is new add it
-
-# @app.route('/signin' , methods=['POST'])
-# def signin():
-#     test=User.query.filter_by(username=request.form['username'])
-#     if test is not None:
-#         resp = make_response(jsonify(success=True))
-#         resp.set_cookie('user_id', user.id)
-#     else: 
-#         app.logger.debug('no username')
-#         resp = make_response(jsonify(success=False))
-#     return resp
-
 
 @app.route('/addproject' , methods=['POST'])
 def add_project():
@@ -193,9 +87,6 @@ def project(title):
 
 
 
-# @app.route('/signup')
-# def signup():
-#     return render_template('signup.html')
 
 
 @app.route('/robots.txt')
