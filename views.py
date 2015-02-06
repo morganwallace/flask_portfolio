237c920888a16dc19978e386ba72225d9e7ac83c
Morgan Wallace <morgan@ischool.berkeley.edu>
2015-02-06 00:41:50 -0800

    debugged flask-login password salting/hashing

--- views.py
+++ views.py
@@ -3,31 +3,25 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 import os,time
 from datetime import date
-#### views (Home, Project pages, etc)
-##############
+
 from app import app
 from manage import db, Project, User
 
 from flask.ext.login import LoginManager, login_user , logout_user , current_user , login_required
- 
+from werkzeug.security import check_password_hash
+
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-from flask.ext.openid import OpenID
-oid = OpenID(app, os.path.join('tmp'))
 
 
-
-
-from forms import LoginForm
-
-# index view function suppressed for brevity
-
+# login setup from tutorial: 
+# https://blog.openshift.com/use-flask-login-to-add-user-authentication-to-your-python-application/
 @app.route('/register' , methods=['GET','POST'])
 def register():
     if request.method == 'GET':
-        return render_template('register.html')
+        return render_template('register.html',pageTitle='register')
     user = User(request.form['username'] , request.form['password'],request.form['email'],request.form['firstname'] ,request.form['lastname'])
     db.session.add(user)
     db.session.commit()
@@ -41,29 +35,27 @@ def login():
         return render_template('login.html')
     username = request.form['username']
     password = request.form['password']
-    registered_user = User.query.filter_by(username=username,password=password).first()
-    if registered_user is None:
+    remember_me = False
+    if 'remember_me' in request.form:
+        remember_me = True
+    registered_user = User.query.filter_by(username=username).first()
+    if registered_user is None or check_password_hash(registered_user.password,password) is False:
         flash('Username or Password is invalid' , 'error')
         return redirect(url_for('login'))
     login_user(registered_user)
     flash('Logged in successfully')
-    return redirect(request.args.get('next') or url_for('index'))
+    return redirect(request.args.get('next') or url_for('home'))
 
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home')) 
 
-######  Miguel Grinberg's login 
+@app.before_request
+def before_request():
+    g.user = current_user
 
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     form = LoginForm()
-#     if form.validate_on_submit():
-#         flash('Login requested for OpenID="%s", remember_me=%s' %
-#               (form.openid.data, str(form.remember_me.data)))
-#         return redirect('/index')
-#     return render_template('login.html', 
-#                            title='Sign In',
-#                            form=form,
-#                            providers=app.config['OPENID_PROVIDERS'])
-
+### End Login views
 
 @app.route('/')
 def home():
